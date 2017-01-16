2a3dac36883b69b6e879352c36f1e27f30205e98
FelixEmmerich <Felix-Emmerich@Freenet.de>
2017-01-16 22:42:38 +0100

    Made functional GazeManager and Cursor classes, about as simply as possible.

---
+++ WEKITPrototypeHL/Assets/Game Mechanism Framework/Scripts/Cursor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameMechanism
+{
+    public class Cursor : MonoBehaviour
+    {
+        public GazeManager GazeManager;
+
+        void Update()
+        {
+            if (GazeManager.hit)
+            {
+                transform.position = GazeManager.HitInfo.point;
+                transform.rotation = Quaternion.FromToRotation(Vector3.up, GazeManager.HitInfo.normal);
+            }
+            else
+            {
+                transform.position = GazeManager.CameraPos + GazeManager.CameraForward * GazeManager.MaxDistance;
+                transform.rotation= Quaternion.identity;
+            }
+        }
+    }
+}
--- WEKITPrototypeHL/Assets/Game Mechanism Framework/Scripts/GazeManager.cs
+++ WEKITPrototypeHL/Assets/Game Mechanism Framework/Scripts/GazeManager.cs
@@ -6,14 +6,37 @@ namespace GameMechanism
 {
     public class GazeManager : MonoBehaviour
     {
-        // Use this for initialization
-        void Start()
-        {
-        }
-
+        public RaycastHit HitInfo;
+        private GameObject LastHit;
+        public float MaxDistance;
+        public bool hit = false;
+        public Vector3 CameraPos;
+        public Vector3 CameraForward;
         // Update is called once per frame
         void Update()
         {
+            CameraPos = Camera.main.transform.position;
+            CameraForward = Camera.main.transform.forward;
+            hit = Physics.Raycast(CameraPos, CameraForward, out HitInfo,
+                MaxDistance);
+            if (hit)
+            {
+                HandleTargets();
+            }
+        }
+
+        void HandleTargets()
+        {
+            GameObject go = HitInfo.collider.gameObject;
+            if (go != LastHit)
+            {
+                LastHit = go;
+                Interactable_Gaze target = LastHit.GetComponent<Interactable_Gaze>();
+                if (target != null)
+                {
+                    target.Enter();
+                }
+            }
         }
     }
 }
