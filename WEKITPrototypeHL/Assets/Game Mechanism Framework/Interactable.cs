8f7799cee22e466049c85a4c3171b5bc0cf60686
FelixEmmerich <Felix-Emmerich@Freenet.de>
2017-01-16 02:01:38 +0100

    I swear I'm figuring stuff out

---
+++ WEKITPrototypeHL/Assets/Game Mechanism Framework/Interactable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GameMechanism
+{
+    public class Interactable : MonoBehaviour
+    {
+        public UnityEvent EnterEvent;
+        public GameObject UiPointerObject;
+
+        private void Awake()
+        {
+            if (!UiPointerObject)
+            {
+                UiPointerObject = Instantiate(UiPointerObject);
+            }
+            else
+            {
+                Debug.Log("No UI Pointer assigned");
+            }
+        }
+
+        public void Enter()
+        {
+            if (EnterEvent!= null)
+            {
+                EnterEvent.Invoke();
+            }
+            if (UiPointerObject)
+            {
+                UiPointerObject.SetActive(false);
+            }
+            gameObject.SetActive(false);
+        }
+    }
+}
