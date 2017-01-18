1d6922a7816d30896d76eb6e4c1639e7b7926ba5
FelixEmmerich <Felix-Emmerich@Freenet.de>
2017-01-18 23:02:06 +0100

    Added GUI to BoxSpawner scene.

---
+++ WEKITPrototypeHL/Assets/Game Mechanism Framework/Scripts/ScanStateGUI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScanStateGUI : MonoBehaviour
+{
+    public Text ScanStateText;
+    public Text StatsText;
+    private bool _scanning;
+    private int _frameCount;
+    private IntPtr _statsPtr;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpatialUnderstanding.Instance.ScanStateChanged += OnStateChange;
+        OnStateChange();
+    }
+
+    void OnStateChange()
+    {
+        if (!_scanning && (int) SpatialUnderstanding.Instance.ScanState > 1)
+        {
+            _scanning = true;
+        }
+        ScanStateText.text = "Scan state: " + SpatialUnderstanding.Instance.ScanState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_scanning)
+        {
+            CheckForStats();
+        }
+    }
+
+    private void CheckForStats()
+    {
+        if (_statsPtr == IntPtr.Zero)
+        {
+            _statsPtr = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceAlignmentPtr();
+        }
+        if (((++_frameCount % 10) == 0) && (SpatialUnderstandingDll.Imports.QueryPlayspaceStats(_statsPtr) != 0))
+        {
+            var stats = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticPlayspaceStats();
+            StatsText.text =
+            (String.Format("total {0} wall {1} horizontal {2} ceiling {3}", stats.TotalSurfaceArea,
+                stats.WallSurfaceArea, stats.HorizSurfaceArea, stats.VirtualCeilingSurfaceArea));
+        }
+    }
+}
