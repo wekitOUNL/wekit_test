b18942b5a0d73e0333ec68d8880e5a68a84ad8df
Marie <ryry.arts@gmail.com>
2017-01-31 16:07:14 +0100

    - (wip) keeping start location persistent through sessions

---
+++ WEKITPrototypeHL/Assets/Scripts/WorldAnchorSetter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA;
+using HoloToolkit.Unity;
+
+public class WorldAnchorSetter : MonoBehaviour
+{
+    public WorldAnchorManager myWAM;
+
+	void Awake ()
+	{
+        myWAM.AttachAnchor(this.gameObject, "me");
+    }
+	
+	void Update ()
+	{
+		
+	}
+}
