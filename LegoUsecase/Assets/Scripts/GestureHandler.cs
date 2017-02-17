a11ed9b983f83070a2a08865c877facdba4a0b96
bibeglimbu <bibeglimbu@gmail.com>
2017-02-17 14:22:46 +0100

    going home update

---
+++ LegoUsecase/Assets/Scripts/GestureHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureHandler : MonoBehaviour {
+    private bool isActive;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isActive)
+        {
+            this.transform.Rotate(0, 1, 0);
+        }
+	}
+
+    void OnAirTapped()
+    {
+        isActive = !isActive;
+    }
+}
